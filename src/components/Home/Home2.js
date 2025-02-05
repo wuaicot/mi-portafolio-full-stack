3151f5224ae1d4f1b207341c924f726bf0c91081
{username} <wuaicot8@gmail.com>
2025-02-05 01:43:08 -0300

    replacing png with webp

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-//import myImg from "../../Assets/avatar.png";
-//import Tilt from "react-parallax-tilt";
+import myImg from "../../Assets/avatar.webp";
+import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
   AiOutlineTwitter,
@@ -54,11 +54,11 @@ function Home2() {
               </i>
             </p>
           </Col>
-          {/* <Col md={4} className="myAvtar">
+          <Col md={4} className="myAvtar">
             <Tilt>
               <img src={myImg} className="img-fluid" alt="avatar" />
             </Tilt>
-          </Col> */}
+          </Col>
         </Row>
         <Row>
           <Col md={12} className="home-about-social">
